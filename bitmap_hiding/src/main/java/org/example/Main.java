9fbafe86e23a5724fbf4a1e3a36687a673ad9991
michelscherer <michel.scherer@students.fhnw.ch>
2025-05-13 20:07:04 +0200

    updated paths and added reveal

--- bitmap_hiding/src/main/java/org/example/Main.java
+++ bitmap_hiding/src/main/java/org/example/Main.java
@@ -5,10 +5,18 @@ import javax.imageio.ImageIO;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedImage coverImage = ImageIO.read(new File("cover.png"));
-        BufferedImage hiddenImage = ImageIO.read(new File("hidden.png"));
+        // Bilder laden
+        BufferedImage coverImage = ImageIO.read(new File("bitmap_hiding/src/main/resources/cover.png"));
+        BufferedImage hiddenImage = ImageIO.read(new File("bitmap_hiding/src/main/resources/hidden.png"));
 
+        // Hiden
         BufferedImage stegoImage = Hider.hideImage(coverImage, hiddenImage);
         ImageIO.write(stegoImage, "png", new File("stego.png"));
+        System.out.println("Bild erfolgreich versteckt und gespeichert als stego.png");
+
+        // Revealen
+        BufferedImage extractedImage = Revealer.revealImage(stegoImage);
+        ImageIO.write(extractedImage, "png", new File("extracted.png"));
+        System.out.println("Verstecktes Bild extrahiert und gespeichert als extracted.png");
     }
 }
