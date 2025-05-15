35c61027375affe9f9b9721e54b9aa8c5ee7dd3b
michelscherer <michel.scherer@students.fhnw.ch>
2025-05-15 20:58:30 +0200

    commented main class

--- bitmap_hiding/src/main/java/org/example/Main.java
+++ bitmap_hiding/src/main/java/org/example/Main.java
@@ -5,17 +5,20 @@ import javax.imageio.ImageIO;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Bilder laden
+        // cover und hidden images aus "ressources" laden
+        // cover ist Trägerbild, hidden das zu verbergende Bild
         BufferedImage coverImage = ImageIO.read(new File("bitmap_hiding/src/main/resources/cover.png"));
         BufferedImage hiddenImage = ImageIO.read(new File("bitmap_hiding/src/main/resources/hidden.png"));
 
-        // Hiden
+        // hider auf cover und hidden anwenden
         BufferedImage stegoImage = Hider.hideImage(coverImage, hiddenImage);
+        // gemergedes Bild "stego" erzeugen
         ImageIO.write(stegoImage, "png", new File("stego.png"));
         System.out.println("Bild erfolgreich versteckt und gespeichert als stego.png");
 
-        // Revealen
+        // revealer auf stego image anwenden
         BufferedImage extractedImage = Revealer.revealImage(stegoImage);
+        // extrahiertes Bild als extracted speichern
         ImageIO.write(extractedImage, "png", new File("extracted.png"));
         System.out.println("Verstecktes Bild extrahiert und gespeichert als extracted.png");
     }
