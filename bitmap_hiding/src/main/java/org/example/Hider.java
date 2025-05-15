1e3a139e944a1082d7532dea0c600d8d6330a924
Olivier Huguenot <olivier.huguenot@students.fhnw.ch>
2025-05-15 21:49:17 +0200

    Added Comments

--- bitmap_hiding/src/main/java/org/example/Hider.java
+++ bitmap_hiding/src/main/java/org/example/Hider.java
@@ -3,27 +3,38 @@ import java.awt.image.BufferedImage;
 
 public class Hider {
     public static BufferedImage hideImage(BufferedImage cover, BufferedImage hidden) {
+        // Neues Bild von gleicher Auflösung generieren
         int width = cover.getWidth();
         int height = cover.getHeight();
         BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
+        // Zeile für Zeile in Bild wird durchlaufen
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
+                // RGB Werte des aktuellen Pixels
                 int coverPixel = cover.getRGB(x, y);
                 int hiddenPixel = hidden.getRGB(x, y);
 
+                // [ 31 - 24 ] [ 23 - 16 ] [ 15 - 8 ] [ 7 - 0 ]
+                // |    A     |     R     |     G    |    B    |
                 int rCover = (coverPixel >> 16) & 0xFF;
                 int gCover = (coverPixel >> 8) & 0xFF;
                 int bCover = coverPixel & 0xFF;
 
+                // [ 31 - 24 ] [ 23 - 16 ] [ 15 - 8 ] [ 7 - 0 ]
+                // |    A     |     R     |     G    |    B    |
+                // MSB wird extrahiert, entweder 0 oder 1
                 int rHiddenBit = ((hiddenPixel >> 16) & 0xFF) >> 7;
                 int gHiddenBit = ((hiddenPixel >> 8) & 0xFF) >> 7;
                 int bHiddenBit = (hiddenPixel & 0xFF) >> 7;
 
+                // Mit 0xFE behalten wir nur die ersten 7 Bits
+                // Anschliessend wird das MSB angehängt
                 rCover = (rCover & 0xFE) | rHiddenBit;
                 gCover = (gCover & 0xFE) | gHiddenBit;
                 bCover = (bCover & 0xFE) | bHiddenBit;
 
+                // Pixel wird zusammengesetzt
                 int mergedPixel = (rCover << 16) | (gCover << 8) | bCover;
                 result.setRGB(x, y, mergedPixel);
             }
